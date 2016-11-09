89d56bf7bcb6e7b62d97a7df891fad1201b6f9e9
root <root@debian>
2016-11-09 15:44:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cas_sso
+=======
+
+A Symfony project created on November 8, 2016, 8:18 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Gess
+ * Date: 08/11/2016
+ * Time: 19:36
+ */
+namespace AppBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
